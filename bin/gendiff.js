171aaf50af61d434a06a373f3c51fc2c8db2fdf7
Artoym1234 <Artoym1234@mail.ru>
2022-11-02 13:22:00 +0300

    step2_gendiff_add_option_-f

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,11 @@ import { Command } from 'commander/esm.mjs';
 const program = new Command();
 program
 .description('Compares two configuration files and shows a difference.')
-.version('0.8.0');
+.version('0.8.0')
+.option( '-f, --format <type>',  'output format')
+.argument('<filepath1>')
+.argument('<filepath2>');
+//const diff = genDiff(filepath1, filepath2);
+//console.log(diff);
 
 program.parse();
