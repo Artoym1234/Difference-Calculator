7647429f26e2637cea1c8ae79e1a7bc95c10951c
Artoym1234 <Artoym1234@mail.ru>
2022-11-07 15:24:28 +0300

    step4

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,24 +1,19 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { test, expect } from '@jest/globals';
+import fs from 'fs';
 import genDiff from '../src/index.js';
 // import getPath from '../src/getPath.js';
 
+// const readFile = (pathToFile) => fs.readFileSync(pathToFile, 'utf-8');
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-
-const first = getFixturePath('file1.json');
-const second = getFixturePath('file2.json');
-
-const expected = `{
-- follow: false
-  host: hexlet.io
-- proxy: 123.234.53.22
-- timeout: 50
-+ timeout: 20
-+ verbose: true
-}`;
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('difference test 1', () => {
-  expect(genDiff(first, second)).toEqual(expected);
+  const first = getFixturePath('file1.json');
+  const second = getFixturePath('file2.json');
+  const testJson = readFile('resultJson.txt');
+  expect(genDiff(first, second)).toEqual(testJson);
 });
